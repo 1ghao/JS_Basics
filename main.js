f1a7dcfc522d7c02940193a127ed47b297d1c63b
1ghao <linwanghao2007@gmail.com>
2023-10-21 12:20:35 +0200

    commit

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 import "./style.css"
 
-
-
-console.log("Hi World");
+function nextInLine(arr, item) {
+    // Only change code below this line
+    let removedItem = arr[0]
+    arr.push(item);
+    arr.shift();
+    return removedItem
+    // Only change code above this line
+  }
+  
+  // Setup
+  let testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
